4f340d77396a91e01942cfa2eb0184ad6d5f5f52
Jitse-Jan <jj.vanwaterschoot@validis.com>
2025-06-05 20:13:40 +0100

    Working Jira pipeline for projects and jql

---
+++ jira_pipeline.py
@@ -0,0 +1,52 @@
+from typing import List, Optional
+
+import dlt
+from jira import jira, jira_search
+
+
+def load(endpoints: Optional[List[str]] = None) -> None:
+    """
+    Load data from specified Jira endpoints into a dataset.
+
+    Args:
+        endpoints: A list of Jira endpoints. If not provided, defaults to all resources.
+    """
+    if not endpoints:
+        endpoints = list(jira().resources.keys())
+
+    pipeline = dlt.pipeline(
+        pipeline_name="jira_pipeline", destination='motherduck', dataset_name="jira"
+    )
+
+    load_info = pipeline.run(jira().with_resources(*endpoints), write_disposition="append")
+
+    print(f"Load Information: {load_info}")
+
+
+def load_query_data(queries: List[str]) -> None:
+    """
+    Load issues from specified Jira queries into a dataset.
+
+    Args:
+        queries: A list of JQL queries.
+    """
+    pipeline = dlt.pipeline(
+        pipeline_name="jira_search_pipeline",
+        destination='motherduck',
+        dataset_name="jira_search",
+    )
+
+    load_info = pipeline.run(jira_search().issues(jql_queries=queries), write_disposition="append")
+
+    print(f"Load Information: {load_info}")
+
+
+if __name__ == "__main__":
+    # Add your desired endpoints to the list 'endpoints'
+    load(endpoints=['projects'])
+
+    queries = [
+        'project = DT',
+    ]
+
+    load_query_data(queries=queries)
